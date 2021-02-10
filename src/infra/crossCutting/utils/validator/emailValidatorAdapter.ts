015a35d062d663c4829bab65b9942898a1246981
g-barbosa <giovanne.bbarbosa@gmail.com>
2021-02-10 18:39:05 -0300

    refactor: move email validator to a folder, create account folder in service domain

--- src/infra/crossCutting/utils/validator/emailValidatorAdapter.ts
+++ src/infra/crossCutting/utils/validator/emailValidatorAdapter.ts
@@ -1,4 +1,4 @@
-import { EmailValidator } from '../protocols/IEmailValidator'
+import { EmailValidator } from '../../protocols/IEmailValidator'
 import validator from 'validator'
 
 export class EmailValidatorAdapter implements EmailValidator {
